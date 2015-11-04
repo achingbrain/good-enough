f4178bb831370c6a970025a78accbf5772d61963
achingbrain <alex@achingbrain.net>
2015-11-04 15:14:13 +0000

    fix up tests for node 0.10

--- test/lib/logger/index.js
+++ test/lib/logger/index.js
@@ -18,8 +18,7 @@ describe('lib/logger', function () {
       './log': logHandler
     })
     logger = new Logger({
-      log: '*',
-      foo: '*'
+      log: '*'
     })
   })
 
@@ -43,17 +42,21 @@ describe('lib/logger', function () {
 
     var stream = through2.obj()
 
+    logger = new Logger({
+
+    })
+
     logger.init(stream, null, sinon.stub())
 
     stream.push({
-      event: 'request',
+      event: 'log',
       tags: [LEVELS.DEBUG, 'foo']
     })
 
     expect(logHandler.called).to.be.false
   })
 
-  it('should log high priority event', function () {
+  it('should log high priority event', function (done) {
     Logger.LEVEL = LEVELS.INFO
 
     var stream = through2.obj()
@@ -65,10 +68,14 @@ describe('lib/logger', function () {
       tags: [LEVELS.WARN]
     })
 
-    expect(logHandler.called).to.be.true
+    setImmediate(function () {
+      expect(logHandler.called).to.be.true
+
+      done()
+    })
   })
 
-  it('should survive untagged event', function () {
+  it('should survive untagged event', function (done) {
     Logger.LEVEL = LEVELS.INFO
 
     var stream = through2.obj()
@@ -79,13 +86,21 @@ describe('lib/logger', function () {
       event: 'log'
     })
 
-    expect(logHandler.called).to.be.true
+    setImmediate(function () {
+      expect(logHandler.called).to.be.false
+
+      done()
+    })
   })
 
   it('should survive event with no handler', function () {
     Logger.LEVEL = LEVELS.INFO
 
     var stream = through2.obj()
+
+    logger = new Logger({
+      foo: '*'
+    })
 
     logger.init(stream, null, sinon.stub())
 
